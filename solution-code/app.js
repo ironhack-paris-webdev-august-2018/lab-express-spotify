f9daf1ec8fbdd08e72ff13332cecdcc269f29cb6
Victor Arza <papu.arza@gmail.com>
2017-09-25 11:51:50 +0200

    new solution added

--- solution-code/app.js
+++ solution-code/app.js
@@ -1,73 +1,67 @@
-const express         = require('express');
-const path            = require('path');
-const favicon         = require('serve-favicon');
-const logger          = require('morgan');
-const bodyParser      = require('body-parser');
-const expressLayouts  = require('express-ejs-layouts');
-const imageCleaner    = require('./helpers/image-cleaner');
-const SpotifyWebApi   = require('spotify-web-api-node');
-const spotify         = new SpotifyWebApi();
-
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+require("dotenv").config();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressLayouts);
-app.use(layoutPicker);
-app.locals.imageCleaner = imageCleaner;
+// Remember to paste here your credentials
+var clientId = process.env.clientId,
+  clientSecret = process.env.clientSecret,
+  accessToken = '';
 
-app.get('/', function(req, res, next) {
-  res.render('index');
+var spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
-app.get('/artists', function(req, res, next) {
-  let searchTerm = req.query.artist;
-
-  spotify.searchArtists(searchTerm, {}, (err, data) => {
-    if (err) throw err;
-
-    let artists = data.body.artists.items;
-
-    res.render('artists/index', {
-      artists: artists,
-      searchTerm: searchTerm
-    });
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
   });
+
+app.get('/', (req, res, next) => {
+  res.render('home');
 });
 
-app.get('/albums/:artistId', function(req, res, next) {
-  let artistId = req.params.artistId;
-
-  spotify.getArtistAlbums(artistId, {}, (err, data) => {
-    if (err) throw err;
-
-    let albums = data.body.items;
-    let artist = albums[0].artists.find(artist => artist.id === artistId);
-
-    res.render('albums/index', {
-      albums: albums,
-      artist: artist.name
+app.get('/artists', (req, res, next) => {
+  spotifyApi.searchArtists(req.query.artist)
+    .then(function(data) {
+      res.render('artists', {
+        artists: data.body.artists.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
     });
-  });
 });
 
-app.get('/tracks/:albumId', function(req, res, next) {
-  let searchTerm = req.params.albumId;
-
-
-  spotify.getAlbumTracks(searchTerm, {}, (err, data) => {
-    if (err) throw err;
-
-    let tracks = data.body.items;
-    res.render('tracks/index', {
-      tracks: tracks,
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+    .then(function(data) {
+      res.render('albums', {
+        albums: data.body.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
     });
-  });
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId)
+    .then(function(data) {
+      res.render('tracks', {
+        tracks: data.body.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
 });
 
 // catch 404 and forward to error handler
@@ -87,14 +81,5 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
-
-function layoutPicker(req, res, next){
-  if (req.path === "/"){
-    app.set('layout', 'layouts/home-layout');
-  } else {
-    app.set('layout', 'layouts/main-layout');
-  }
-  next();
-}
 
 module.exports = app;
--- solution-code/helpers/image-cleaner.js
+++
@@ -1,9 +0,0 @@
-function imageCleaner(spotifyObject){
-  if (spotifyObject.images[0]){
-    return spotifyObject.images[0].url;
-  } else {
-    return "https://placeholdit.imgix.net/~text?txtsize=33&txt=250%C3%97250&w=250&h=250"
-  }
-}
-
-module.exports = imageCleaner;
